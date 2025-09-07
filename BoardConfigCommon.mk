389798c58d20c842ee68d1c3a69bcf71b07d2ec7
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 12:49:31 +0000

    Adapt to LMODroid by device-add script

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -152,7 +152,7 @@ BOARD_HAVE_QCOM_FM := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(VENDOR_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/lmodroid/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
 
